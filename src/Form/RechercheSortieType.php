760a17623d75f59efe4379b345dc8ff2eccb1325
Taorron <lucas.benjamin4@gmail.com>
2022-03-04 13:59:44 +0100

    main ajouter les filtres sur la page des sorties

--- src/Form/RechercheSortieType.php
+++ src/Form/RechercheSortieType.php
@@ -6,29 +6,14 @@ use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class RechercheSortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('lieu',null,
-            ['label'=>'Site : ',
-            'attr' => [
-                'class' => 'form-control'
-            ]])
-        ->add('nom',null,['label'=>'Nom de la sortie contient : ',
-            'attr' => [
-                'class' => 'form-control'
-            ]])
-        ->add('dateDebut',null,['label'=>'Entre : ',
-            'attr' => [
-                'class' => 'form-control'
-            ]])
-        ->add('dateCloture',null,['label'=>'et : ',
-            'attr' => [
-                'class' => 'form-control'
-            ]])
+  
         ;
     }
 
